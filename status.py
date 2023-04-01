f6b91ed4831e50cfd25ac92951efd6aca7afa471
Cheolseung Jung <jungcheolseung@gmail.com>
2023-04-01 17:59:38 +0900

    Add PlayerState and GameState

--- status.py
+++ status.py
@@ -1,7 +1,15 @@
 import random
 from abc import ABC, abstractmethod
-from enum import Enum, auto
+from enum import Enum, auto, IntEnum
 from dataclasses import dataclass, field
+from typing import Self
+
+
+class Player(IntEnum):
+    _1 = 0
+    _2 = 1
+    _3 = 2
+    _4 = 3
 
 
 class AgentIcon(Enum):
@@ -35,17 +43,17 @@ class Card(ABC):
 @dataclass
 class Imperium(Card):
     name: str
-    faction: list[Faction]
-    agent_box: list
-    reveal_box: list
-    persuasion_cost: int
-    acquire_effect: list
+    faction: list[Faction] = field(default_factory=list)
+    agent_box: list = field(default_factory=list)
+    reveal_box: list = field(default_factory=list)
+    persuasion_cost: int = 0
+    acquire_effect: list = field(default_factory=list)
     agent_icons: list[AgentIcon] = field(default_factory=list)
     starting: bool = False
     reveal_persuation: int = 0
     reveal_swords: int = 0
 
-    def __str__(self):
+    def __str__(self) -> str:
         return self.name
 
 
@@ -68,44 +76,115 @@ class Deck:
     def __len__(self) -> int:
         return len(self.deck)
 
-    def add_card(self, card: Card):
+    def add_card(self, card: Card) -> None:
         self.deck.append(card)
 
     def pop_card(self) -> Card:
         return self.deck.pop()
 
-    def shuffle(self):
+    def shuffle(self) -> None:
         random.shuffle(self.deck)
+
+    def discard(self, index: int) -> Card:
+        return self.deck.pop(index)
+
+    def random_discard(self) -> Card:
+        return self.discard(random.randrange(len(self.deck)))
+
+    def swap(self, i: int, j: int) -> None:
+        self.deck[i], self.deck[j] = self.deck[j], self.deck[i]
+
+    def move(self, i: int, to: Self) -> None:
+        to.add_card(self.discard(i))
 
     def __repr__(self) -> str:
         return repr(self.deck)
 
 
-class Hand(Deck):
-    def __init__(self) -> None:
-        super().__init__()
-
-    def discard(self, index: int) -> Card:
-        return self.deck.pop(index)
+class Unit:
+    troop: int = 0
+    dreadnought: int = 0
 
 
 @dataclass
-class Player:
-    imperium: Hand = field(default_factory=Hand)
-    intrigue: Hand = field(default_factory=Hand)
-    water: int = 1
-    solari: int = 0
+class PlayerState:
+    hand: Deck = field(default_factory=Deck)
+    intrigue: Deck = field(default_factory=Deck)
+    deck: Deck = field(default_factory=Deck)
+    in_play: Deck = field(default_factory=Deck)
+    discard_pile: Deck = field(default_factory=Deck)
+    score: int = 0
     spice: int = 0
-    agents: int = 2
-    cubes: int = 16
+    solari: int = 0
+    water: int = 1
+    agent: int = 2
+    troop: int = 12
+    influence: dict = field(default_factory=lambda: {
+        Faction.EMPEROR: 0,
+        Faction.SPACING_GUILD: 0,
+        Faction.BENE_GESSERIT: 0,
+        Faction.FREMEN: 0
+    })
+    garrison: Unit = field(default_factory=Unit)
+    conflict: Unit = field(default_factory=Unit)
+    council_seat: bool = False
 
 
-if __name__ == '__main__':
-    i = Imperium('test', [Faction.BENE_GESSERIT, Faction.EMPEROR], [], [], 2, [], [AgentIcon.LANDSRAAD])
-    print(i)
-    test_hand = Hand()
-    test_hand.add_card(i)
-    player_1 = Player(imperium=test_hand, water=1)
-    print(player_1)
-    print(player_1.imperium.pop_card())
-    print(player_1)
+class GameState:
+    def __init__(self, num_players: int, mode) -> None:
+        self.num_players = num_players
+        self.mode = mode
+        self.imperium_deck: Deck = Deck()
+        self.imperium_row: Deck = Deck()
+        self.imperium_discarded: Deck = Deck()
+        self.conflict_deck: Deck = Deck()
+        self.conflict_discarded: Deck = Deck()
+        self.intrigue_deck: Deck = Deck()
+        self.intrigue_discarded: Deck = Deck()
+        self.mentat: bool = True
+
+    def init_players(self) -> None:
+        pass
+
+    def init_decks(self) -> None:
+        pass
+
+
+@dataclass
+class BoardSpaceCost:
+    spice: int = 0
+    solari: int = 0
+    water: int = 0
+
+
+@dataclass
+class BoardSpace:
+    name: str
+    agent_icon: AgentIcon
+    combat_space: bool
+    cost: BoardSpaceCost = field(default_factory=BoardSpaceCost)
+    # gain
+    faction: bool = field(init=False)
+
+    def __post_init__(self):
+        self.faction = self.agent_icon.name in Faction.__members__
+
+
+class Leader:
+    def __init__(self, name) -> None:
+        self.name = name
+
+
+
+# if __name__ == '__main__':
+    # i = Imperium('test', [Faction.BENE_GESSERIT, Faction.EMPEROR], [], [], 2, [], [AgentIcon.LANDSRAAD])
+    # print(i)
+    # test_hand = Deck()
+    # test_hand.add_card(i)
+    # player_1 = Player(imperium=test_hand, water=1)
+    # print(player_1)
+    # print(player_1.imperium.pop_card())
+    # print(player_1)
+    # a = [0,1,1,1]
+    # print(a[Player._4])
+    # print(AgentIcon.EMPEROR.name in Faction.__members__)
